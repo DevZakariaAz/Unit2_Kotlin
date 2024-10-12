eeb102c4dcd479db751abcfb1423ac61aafed45c
Dev Zakaria <Azizi.zakaria.solicode@gmail.com>
2024-10-12 05:03:04 +0100

    Tuto else if

---
+++ Tuto/Tuto_When.kt
@@ -0,0 +1,13 @@
+// Tuto about Using When in kotlin
+
+fun main() {
+    val Color = "Blue"
+
+    val message = when(Color) {
+        "Red" -> "Color is Red"
+        "Yellow" -> "Color is Yellow."
+        "Green" -> "Color is Green"
+        else -> "We don't now !!"
+    }
+    println(message)
+}
---
+++ Tuto/Tuto_else_if.kt
@@ -0,0 +1,17 @@
+// Tuto about Using if else in kotlin
+
+fun main() {
+    val Color = "Green"
+    if (Color == "Red"){
+           println("Color is Red")
+    }
+    else if(Color == "Green"){
+        println("Color is Green")
+    }
+    else if(Color == "Yellow"){
+        println("Color is Yellow")
+    }
+    else{
+           println("We don't now !!")
+    }
+}
