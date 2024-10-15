3101fa5d103d822a5e8be62dfe486472917266a2
Dev Zakaria <Azizi.zakaria.solicode@gmail.com>
2024-10-15 20:36:34 +0100

    Tuto delegues

---
+++ Tuto/delegues.kt
@@ -0,0 +1,16 @@
+interface  Base {
+    fun print()
+}
+open class BaseImpl(val text: String) : Base{
+    override fun print() {
+        print(text)
+    }
+}
+
+class drived(b: Base) : Base by b
+
+
+fun main() {
+    val basse = BaseImpl("Hello delegation")
+    drived(basse).print()
+}
